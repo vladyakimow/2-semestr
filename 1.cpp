134300b62f5c9607021e64d7e04f3b55f109daba
Vlad_Yakimow <31766832+vladyakimow@users.noreply.github.com>
2018-04-05 16:38:57 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,35 +4,46 @@ using namespace std;
 class hero{
 private:
     int live;
+    string name;
+protected:
     int xp;
     double money;
-    string name;
 public:
+    static int count;
     hero(){
-    live=5;
-    xp=0;
-    money=625;
-    name="ursa";
+    this->live=5;
+    this->xp=0;
+    this->money=625;
+    this->name="ursa";
+    this->count++;
     }
     hero(int l,int x,double m,string n){
-    live=l;
-    xp=x;
-    money=m;
-    name=n;
-    }    
+    this->live=l;
+    this->xp=x;
+    this->money=m;
+    this->name=n;
+    this->count++;
+    }
     //distruktor
     ~hero(){
-    name="abbadon";
+    this->name="abbadon";
+    this->count--;
     }
     void getinfo (){
-    cout << name << endl << live << endl << xp << endl << money << endl; 
+    cout << name << endl << live << endl << xp << endl << money << endl;
+
     }
 };
 
+int hero::count = 0;
+
 int main(){
+cout << "Count: " << hero::count << "\n";
 hero h1;
+cout << "Count: " << hero::count << "\n";
 hero h2(100,50,800,"invoker");
 h1.getinfo();
 h2.getinfo();
+cout << "Count: " << hero::count << "\n";
 return 0;
 }
